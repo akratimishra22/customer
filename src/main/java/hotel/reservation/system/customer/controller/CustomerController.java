3205e26ea494ad5cf118188f758eb9b4f3559a6d
Akrati Mishra <akrati.mishra@bt.com>
2023-10-15 16:10:50 +0100

    Add sign up functionality

--- src/main/java/hotel/reservation/system/customer/controller/CustomerController.java
+++ src/main/java/hotel/reservation/system/customer/controller/CustomerController.java
@@ -3,11 +3,15 @@ package hotel.reservation.system.customer.controller;
 import hotel.reservation.system.customer.model.Customer;
 import hotel.reservation.system.customer.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -15,6 +19,13 @@ public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
+    private final RestTemplate restTemplate;
+    private final String notificationsBaseUrl;
+
+    public CustomerController(RestTemplate restTemplate, @Value("${notifications.base-url}") String notificationsBaseUrl) {
+        this.restTemplate = restTemplate;
+        this.notificationsBaseUrl = notificationsBaseUrl;
+    }
 
     @PostMapping("/register")
     public ResponseEntity<Customer> registerCustomer(@Valid @RequestBody Customer customer) {
@@ -33,6 +44,30 @@ public class CustomerController {
             return new ResponseEntity<>(customer, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Customer> signUpCustomer(@Valid @RequestBody Customer customer) {
+        try {
+            Customer createdCustomer = customerService.createCustomer(customer);
+
+            if (createdCustomer != null) {
+                Map<String, String> requestBody = new HashMap<>();
+                requestBody.put("message", "Welcome to our platform, " + createdCustomer.getName());
+
+                ResponseEntity<String> response = restTemplate.postForEntity(notificationsBaseUrl + "/send", requestBody, String.class);
+
+                if (response.getStatusCode() == HttpStatus.CREATED) {
+                    return new ResponseEntity<>(createdCustomer, HttpStatus.CREATED);
+                } else {
+                    return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+            } else {
+                return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
